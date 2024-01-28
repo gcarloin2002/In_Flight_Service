4390149d419c9b434cc88205d26b0d4a63433061
gcarloin2002 <gcarloin2002@gmail.com>
2024-01-28 10:53:16 -0600

    adjusted checkout page

--- app/food-order/checkout/page.js
+++ app/food-order/checkout/page.js
@@ -1,3 +1,23 @@
+
+import confirmed from "../../../assets/confirmed.png";
+import "../../../styles/Checkout.css";
+import ChevronLeft from "../../../assets/chevron-left-solid.svg";
+import Link from "next/link";
+
 export default function Checkout() {
-  return <div>Confirm Checkout</div>;
+  return (
+    <div className="food-order-container">
+      <div className="header">
+        <Link href="/">
+          <img className="chevron-left" src={ChevronLeft.src} />
+        </Link>
+      </div>
+      <div className="confirmBox">
+
+        <img className="confirmedImg" src={confirmed.src} />
+        <div className="confirmMessage"> Order Confirmed! </div>
+      </div>
+      <div className="header"></div>
+    </div>
+    );
 }
