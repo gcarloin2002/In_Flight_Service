b8e963bc9f1b24c5f1ef27b9e70ef577fb4db70c
gcarloin2002 <gcarloin2002@gmail.com>
2024-01-27 19:53:02 -0600

    updated homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,24 +1,37 @@
 import Link from "next/link";
-import AA_Logo from "../assets/AA_Logo.png";
+import food_icon from "../assets/food_icon.png";
+import restroom_icon from "../assets/restroom_icon.png"
 import "../styles/Home.css";
 
 // <img className="AA_Logo" src={AA_Logo.src}/>
 
 export default function Home() {
     return (
-      <>
+      <div className="food-order-container">
 
         <div className="header">
         
         </div>
 
         <div className="buttonSet">
-            <div className="menuButton">
-                
-            </div>
-            <div className="menuButton">
-
-            </div>
+            <Link href="/restroom">
+                <div className="menuButton">
+                    <img className="icon" src={restroom_icon.src}/>
+                    <div className="buttonTitle">Restroom</div>
+                    <div className="redLine"> </div>
+                    <p>Order food now and your order will arrive next round</p>
+                    <div className="redBox">Reserve</div>
+                </div>
+            </Link>
+            <Link href="/food-order">
+                <div className="menuButton">
+                    <img className="icon" src={food_icon.src}/>
+                    <div className="buttonTitle">Food Order</div>
+                    <div className="blueLine"> </div>
+                    <p>Check restroom availability and reserve a time </p>
+                    <div className="blueBox">Order Now</div>
+                </div>
+            </Link>
         </div>
 
         <div className="footer">
@@ -27,6 +40,6 @@ export default function Home() {
             
 
         
-      </>
+      </div>
     )
   }
