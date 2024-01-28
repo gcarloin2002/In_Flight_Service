47f936e133900512ab7a202614fa6b5871e63253
gcarloin2002 <gcarloin2002@gmail.com>
2024-01-28 00:12:45 -0600

    updated restroom page

--- frontend/app/restroom/page.js
+++ frontend/app/restroom/page.js
@@ -1,3 +1,38 @@
+import ChevronLeft from "../../assets/chevron-left-solid.svg";
+import Link from "next/link";
+import "../../styles/Restaurant.css";
+
 export default function Restroom() {
-  return <div>Restroom</div>;
+  return (
+    <div className="food-order-container">
+      <div className="header">
+        <Link href="/">
+          <img className="chevron-left" src={ChevronLeft.src} />
+        </Link>
+
+      </div>
+      <div className="centralContainer">
+        <div className="buttonCol">
+          <div className="reserveButton">
+
+          </div>
+          <div className="reserveButton">
+
+          </div>
+        </div>
+        
+        <div className="buttonCol">
+          <div className="reserveButton">
+
+          </div>
+          <div className="reserveButton">
+
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+
+      </div>
+    </div>
+  )
 }
