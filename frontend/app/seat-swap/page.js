b34679b6034c30cfd54a295abe8e1ce1bc042f70
Ilham Aryawan <ia601612@tamu.edu>
2024-01-28 03:59:37 -0600

    Able to add orders

--- frontend/app/seat-swap/page.js
+++ frontend/app/seat-swap/page.js
@@ -1,3 +1,40 @@
-export default function SeatSwao() {
-  return <div>Seat Swap</div>;
+"use client"
+
+export default function SeatSwap() {
+    return (
+        <div>
+          <h1>Seat Swap</h1>
+          <button onClick={() => addOrder()}>Button 1</button>
+          <button onClick={() => handleClick("Button 2")}>Button 2</button>
+          <button onClick={() => handleClick("Button 3")}>Button 3</button>
+        </div>
+      );
 }
+
+function handleClick(buttonName) {
+    console.log(`Clicked ${buttonName}`);
+    // You can add your logic here for each button click
+  }
+
+async function addOrder() {
+    const newOrder = { id: 4, userid: 0, orderconfirmed: false };
+    try {
+        const response = await fetch('/api/addOrder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                newOrder,
+            }),
+        });
+        const data = await response.json(); // await here
+
+        console.log(data);
+
+        // You might want to handle the response here if needed
+    } catch (error) {
+        console.error('Error updating row:', error);
+    }
+}
+
