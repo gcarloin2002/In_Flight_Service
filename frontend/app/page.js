2bd3814b5cc958502acfc17fe6e8d06c3fb453f1
gcarloin2002 <gcarloin2002@gmail.com>
2024-01-27 18:05:08 -0600

    style change

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,16 +1,26 @@
 import AA_Logo from "../assets/AA_Logo.png";
 import "../styles/Home.css";
 
+// 
+
 export default function Home() {
     return (
       <>
-      <div className="homeLayout">
-        <div className="header">
-            <img className="AA_Logo" src={AA_Logo.src}/>
-        </div>
-        
 
-      </div>
+        <div className="header">
+        <img className="AA_Logo" src={AA_Logo.src}/>
+        </div>
+
+        <div className="middleLayout">
+
+        </div>
+
+        <div className="footer">
+
+        </div>
+            
+
+        
       </>
     )
   }
