f7c7dbb8c620f6bd5e18329665be72781ba3116e
gcarloin2002 <gcarloin2002@gmail.com>
2024-01-27 18:18:29 -0600

    updated styling

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,18 +1,23 @@
 import AA_Logo from "../assets/AA_Logo.png";
 import "../styles/Home.css";
 
-// 
+// <img className="AA_Logo" src={AA_Logo.src}/>
 
 export default function Home() {
     return (
       <>
 
         <div className="header">
-        <img className="AA_Logo" src={AA_Logo.src}/>
+        
         </div>
 
-        <div className="middleLayout">
+        <div className="buttonSet">
+            <div class="menuButton">
+                
+            </div>
+            <div class="menuButton">
 
+            </div>
         </div>
 
         <div className="footer">
